7facf5e42e2f2b3461fbd82f80548fa5692fed29
agustin susilowati <agustin@agustins-macbook-pro.local>
2019-01-03 17:54:28 +0700

    Initial Commit

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by agustin susilowati on 03/01/19.
+//  Copyright © 2019 Angie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
