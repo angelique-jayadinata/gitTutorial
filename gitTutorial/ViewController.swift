fd74804bf0321bed547b7027743692000262568d
agustin susilowati <agustin@agustins-macbook-pro.local>
2019-01-03 18:14:07 +0700

    hello again

--- gitTutorial/ViewController.swift
+++ gitTutorial/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    func reverse(text: String) -> String {
+        return String(text.reversed())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 }
